c0959e4de176bc683472bb72ac95a4ee9f9d995e
crimsondrac1 <doug@chillopolis.com>
2022-06-08 21:09:51 -0500

    URL Update

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,4 @@
-const socket = io('http://localhost:3001')
+const socket = io('https://livechat-app-1.herokuapp.com')
 const messageContainer = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
